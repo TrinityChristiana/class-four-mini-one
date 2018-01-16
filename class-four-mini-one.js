68bd86547a14702f3b78ba3c3e6f61b2cd5325ce
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2018-01-15 18:38:48 -0600

    1/15/18

---
+++ class-four-mini-one.js
@@ -0,0 +1,17 @@
+
+// 1. Write (declare) a function named hereIsAJoke. This function should console log a joke you made up whenever I call it.
+function hereIsAJoke(){
+    console.log(`Random Joke Here`)
+  }
+  // 2. Call (execute) your function tellMeImCool.
+  hereIsAJoke();
+
+
+
+
+// 1. Write (declare) a function named tellMeImCool. This function should console log "You're so cool." whenever I call it.
+function tellMeImCool() {
+    console.log(`You're so cool`)
+  }
+  // 2. Call (execute) your function tellMeImCool.
+  tellMeImCool();
